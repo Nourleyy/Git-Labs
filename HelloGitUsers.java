ca9c8a7aa0fdca64f9006f9ff7fe65535d892fba
Nourleyy <nourantamer077@gmail.com>
2025-01-06 12:35:07 +0200

    Added files

---
+++ HelloGitUsers.java
@@ -0,0 +1,14 @@
+public class HelloGitUsers {
+    public void printHelloITI() {
+        System.out.println("Hello, ITI!");
+    }
+    public void printHelloGit() {
+        System.out.println("Hello, Git!");
+    }
+
+    public static void main(String[] args) {
+        HelloGitUsers helloITI = new HelloGitUsers();
+        helloITI.printHelloITI();
+        helloITI.printHelloGit();
+    }
+}
---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava {
+    public void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        HelloJava helloWorld = new HelloJava();
+        helloWorld.printHelloWorld();
+    }
+}
